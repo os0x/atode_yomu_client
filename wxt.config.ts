c8caa31aed882bd183243a9c3babe5c7d9f726b7
shogo-ohta <shogo-ohta@cookpad.com>
2024-10-10 01:03:17 +0900

    Remove unneeded host_permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,10 +4,9 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
     name: 'atode yomu client',
-    version: '0.0.1',
+    version: '0.0.2',
     description: 'Easily add to atode yomu',
     permissions: ['storage', 'activeTab'],
-    host_permissions: ['https://atodeyomu.morishin.me/'],
     action: {
       default_popup: 'popup.html',
       default_icon: {
